25cb35f24a72f2a34632f575cb93204bee604f68
Adeyoriju Olabode <whilson03@gmail.com>
2023-06-01 19:44:15 +0100

    code refactor

---
+++ sentences.py
@@ -0,0 +1,51 @@
+
+correct_english_sentences = [
+    "My wife is a kind person.",
+    "They are proud parents of twins.",
+    "My sister is a talented musician.",
+    "My brother is a great father."
+]
+
+incorrect_english_sentences = [
+    "My wife is a kind trait.",
+    "They is proud parents of twins.",
+    "Our family is planning a vacation.",
+    "My sister is a talented musicians.",
+    "My brother is a great fathers."
+]
+
+correct_yoruba_sentences = [
+    "ìyá mi ti jẹún ní òjú ọ́nà.",
+    "Mo jẹún",
+    "Ọmọ mi ti wà lára ilé.",
+    "Bàbá mi ní ọkọ́ mẹ́tà.",
+    "Ẹ̀yin ni èrò ẹni nínú ilé."
+]
+
+incorrect_yoruba_sentences = [
+    "Iyá mi ti wá jẹun lọ́nà.",
+    "Mo jẹún ni àtẹ́lẹwọ́ ni òjú ewe.",
+]
+
+correct_tiv_sentences = [
+    "kwase wan bee iyol.",
+    "wanye wan tema sha kyon",
+
+]
+
+incorrect_tiv_sentences = [
+    "Nyin ver in kwagh.",
+    "wanye wan doo or",
+
+]
+
+correct_efik_sentences = [
+    "Mkpò eti bó.",
+    "Nse ubok bó.",
+]
+
+incorrect_efik_sentences = [
+    "Nse inua ófen.",
+    "Mkpò eti bó me ubok.",
+    "Nse inua idip me eti."
+]
